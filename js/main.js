83628de2940e3f730830f802f104a7fff4863b5a
Rosineia <neyasantoss@gmail.com>
2021-08-24 00:51:47 -0300

    aula JS

---
+++ js/main.js
@@ -0,0 +1,57 @@
+//var nome = 'Rosineia Santos'
+//var idade=29
+//var frase='Japão é o melhor time do mundo'
+
+//alert('Bem vinda!!' + " " + nome)
+
+//alert(nome +" "+ "tem"+ " "+ idade +" "+ 'anos')
+
+//alert("meu primeiro js"); //posso criar um alerta na paSTA html, ou JS
+
+//console.log(frase.replace('Japão', "Brasil"))
+
+//Array
+//var lista =['maça', "pera","laranja", 'Rosineia Santos',29]
+//lista.push("uva");// inserir um elemento
+//lista.pop();
+//console.log(lista)
+//console.log(lista.toString()); //tira do array
+//console.log(lista.join(" - "));//
+//alert(lista[3])
+
+//DICIONARIO
+
+/*var fruta= {nome: 'Maça', cor:'vermelha'}
+console.log(fruta.nome)
+alert(fruta.cor)*/
+
+//Lista de dicionario
+//var frutas= [{nome: 'Maça', cor:'vermelha'}, {nome: 'uva', cor:'vinho'}]
+//console.log(frutas.nome)
+//alert(frutas[1].nome)
+
+//Condicionais
+//var idade= prompt('Qual sua idade?'); //prompt emibe pergunta para usuario
+//var idade= 17;
+/*if(idade >= 18){
+    alert('Maior de idade')
+}else{
+    alert('Menor de idade')
+}*/
+
+/*Repetição
+
+var count = 0;
+while(count<=5){
+    console.log(count)
+    count = count + 1//posso usar count ++
+}
+
+var count= count;
+for(count = 0; count<=5; count ++){
+    alert(count)
+}*/
+
+//Datas
+ var d= new Date();
+ alert(d.getMonth()+1)
